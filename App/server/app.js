6631156cc842a086d88022d5042bdf65ce01f314
Collin Andriessen <candriessen@st.noorderpoort.nl>
2022-03-16 14:20:58 +0100

    server opzet

---
+++ App/server/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+require("dotenv").config();
+
+const app = express();
+
+app.listen(5000, () => console.log("Server started"));
+
+mongoose.connect(process.env.MONGODB_CONNECT, (err) => {
+    if (err) return console.log(err);
+    console.log("Connected to the db");
+  });
+
+app.use(cookieParser());
+app.use(require("morgan")("tiny"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
