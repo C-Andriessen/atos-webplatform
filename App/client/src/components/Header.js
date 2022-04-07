fd30a8909b6a9e8d8d0c6cb2b647460e7747d25b
Collin Andriessen <candriessen@st.noorderpoort.nl>
2022-04-07 09:45:19 +0200

    light en darkmode (geen werkende button nog) + error message

--- App/client/src/components/Header.js
+++ App/client/src/components/Header.js
@@ -1,22 +1,41 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg'
 
 function Header () {
+
+    const [mode, setMode] = useState('dark');
+
+
+    if (mode === 'light') {
     return <header>
         <img src={logo} alt="logo" />
-        {/* options */}
         <nav className='header'>
             <Link to='/'>Home</Link>
             <Link to='/explanation'>Hoe werkt het</Link>
             <Link to='/excercises'>Opdrachten</Link>
             <Link to='/students'>Studenten</Link>
         </nav>
-        {/* login/register */}
         <nav className='header'>
             <Link to='/register'>Registreren</Link>
             <Link to='/login' className='login-button'>Inloggen</Link>
         </nav>
     </header>
+    } else if (mode === 'dark') {
+        return <header>
+        <img src={logo} alt="logo" />
+        <nav className='header'>
+            <Link to='/' className='darkmode'>Home</Link>
+            <Link to='/explanation' className='darkmode'>Hoe werkt het</Link>
+            <Link to='/excercises' className='darkmode'>Opdrachten</Link>
+            <Link to='/students' className='darkmode'>Studenten</Link>
+        </nav>
+        <nav className='header'>
+            <Link to='/register' className='darkmode'>Registreren</Link>
+            <Link to='/login' className='login-button darkmode'>Inloggen</Link>
+        </nav>
+    </header>
+    }
 }
 
 export default Header;
--- App/client/src/components/Home/Landing.js
+++ App/client/src/components/Home/Landing.js
@@ -1,32 +1,117 @@
+import {useState} from 'react';
 import hero_img from './hero.jpg';
 import search_icon from './search-icon.svg'
 
 function Landing () {
-    return (
-        <div className='landing'>
+
+    const [mode, setMode] = useState('dark')
+
+    const [location, setLocation] = useState('')
+    const [service, setService] = useState('')
+    const [school, setSchool] = useState('')
+
+    const [errorLocation, setErrorLocation] = useState('');
+    const [errorService, setErrorService] = useState('');
+    const [errorSchool, setErrorSchool] = useState('');
+
+    function handleLocationChange (ev) {
+        setLocation(ev.target.value);
+    }
+
+    function handleServiceChange (ev) {
+        setService(ev.target.value);
+    }
+
+    function handleSchoolChange (ev) {
+        setSchool(ev.target.value);
+    }
+
+    function handleSubmit(ev) {
+        ev.preventDefault();
+        if (location.length === 0) {
+            setErrorLocation('Vul de locatie in')
+        } else {
+            setErrorLocation('')
+        }
+        if (service.length === 0) {
+            setErrorService('Vul in wat voor klus u zoekt')
+        } else {
+            setErrorService('')
+        }
+        if (school.length === 0) {
+            setErrorSchool('Vul uw voorkeurs school in')
+        } else {
+            setErrorSchool('')
+        }
+        // alert(school)
+        // alert(location)
+        // alert(service)
+    }
+
+
+    if (mode === 'light') {
+        return (
+            <div className='landing'>
             <div className='text-section'>
-                <h1 className='landing-text'>Zoek een <span className='text-purple'>student</span> die past bij uw <span className='text-purple'>klus</span></h1>
+                <h1 className='landing-text-light'>Zoek een <span className='text-purple'>student</span> die past bij uw <span className='text-purple'>klus</span></h1>
                 <p>Vul hier de locatie in waar u bent en wat 
                     de klus inhoud waar u hulp bij nodig heeft</p>
-                <form method='get' action='#' className='hero-form'>
+                <form method='get'  className='hero-form' onSubmit={handleSubmit}>
                     <div className='flex-column'>
                     <label>Locatie</label>
-                    <input name='locatie' value='Groningen' />
+                    <input name='locatie' value={location} onChange={handleLocationChange} placeholder='Groningen' />
+                    <span className='error'>{errorLocation}</span>
                     </div>
                     <div className='flex-column px-3 line-left'>
                         <label>Welke service</label>
-                    <input name='service' value='Software developer' />
+                    <input name='service' value={service} onChange={handleServiceChange} placeholder='Software Developer' />
+                    <span className='error'>{errorService}</span>
                     </div>
                     <div className='flex-column line-left pl-2'>
                     <label>Voorkeur school</label>
-                    <input name='school' value='School' />
+                    <input name='school' value={school} onChange={handleSchoolChange} placeholder='Noorderpoort' />
+                    <span className='error'>{errorSchool}</span>
                     </div>
-                    <button><img src={search_icon} alt='zoeken'></img></button>
+                    <button type='submit' className='ml-2'><img src={search_icon} alt='zoeken'></img></button>
                 </form>
             </div>
                 <img src={hero_img} alt="logo" className='hero'/>
         </div>
-    )
+        )
+    } else if (mode === 'dark') {
+        const body = document.querySelector('body');
+        body.style.backgroundColor = '#2F3136';
+
+        return (<>
+            <div className='landing'>
+            <div className='text-section'>
+                <h1 className='landing-text darkmode'>Zoek een <span className='text-purple'>student</span> die past bij uw <span className='text-purple'>klus</span></h1>
+                <p className='darkmode'>Vul hier de locatie in waar u bent en wat 
+                    de klus inhoud waar u hulp bij nodig heeft</p>
+                <form method='get'  className='hero-form darkmode' onSubmit={handleSubmit}>
+                    <div className='flex-column'>
+                    <label>Locatie</label>
+                    <input name='locatie' value={location} onChange={handleLocationChange} placeholder='Groningen' />
+                    <span className='error'>{errorLocation}</span>
+                    </div>
+                    <div className='flex-column px-3 line-left'>
+                        <label>Welke service</label>
+                    <input name='service' value={service} onChange={handleServiceChange} placeholder='Software Developer' />
+                    <span className='error'>{errorService}</span>
+                    </div>
+                    <div className='flex-column line-left pl-2'>
+                    <label>Voorkeur school</label>
+                    <input name='school' value={school} onChange={handleSchoolChange} placeholder='Noorderpoort' />
+                    <span className='error'>{errorSchool}</span>
+                    </div>
+                    <button type='submit' className='ml-2'><img src={search_icon} alt='zoeken'></img></button>
+                </form>
+            </div>
+                <img src={hero_img} alt="logo" className='hero'/>
+        </div>
+        </>
+        )
+    }
 }
 
 export default Landing;
