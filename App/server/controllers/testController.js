50f0ccda846117a9a7e81aab9174df851ad2f350
Collin Andriessen <candriessen@st.noorderpoort.nl>
2022-04-07 15:28:30 +0200

    kleine aanpassing

--- App/server/controllers/testController.js
+++ App/server/controllers/testController.js
@@ -2,9 +2,4 @@ async function test(req, res) {
 
     res.send("De connectie tussen frontend en backend werkt met axios");
 }
-
-async function cookie(req, res) {
-    
-}
-
 module.exports = {test, cookie}
