4afafa46bd10f63916ade8cee77c75125ea4636e
Collin Andriessen <candriessen@st.noorderpoort.nl>
2022-05-31 22:50:35 +0200

    changed host

--- App/client/src/context/hostContext.js
+++ App/client/src/context/hostContext.js
@@ -3,7 +3,7 @@ import React, { useState, createContext } from "react";
 const hostContext = createContext();
 
 const HostContextProvider = ({ children }) => {
-  const [host] = useState("http://atos.c-demo.dev/server");
+  const [host] = useState("https://atos.c-demo.dev/server");
 
   return <hostContext.Provider value={host}>{children}</hostContext.Provider>;
 };
